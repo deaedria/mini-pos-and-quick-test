89c84d7f95b79a2586e816b838051e1ec1a7bbc4
Dea Edria <73749289+deaedria@users.noreply.github.com>
2021-09-29 12:21:02 +0700

    Delete src directory

--- src/Services/transactionService.js
+++
@@ -1,9 +0,0 @@
-// FUNCTION TO ADD BILLING IN LOCALSTORAGE
-export function addBillingToLocalStorage(data) {
-    return localStorage.setItem("BillingPOS", JSON.stringify(data));
-}
-
-// FUNCTION TO GET BILLING IN LOCALSTORAGE
-export function getBillingLocalStorage() {
-    return JSON.parse(localStorage.getItem("BillingPOS"));
-}
